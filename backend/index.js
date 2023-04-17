c9cbd0e2f0f56cbf11e9e4e2a52f7302de84231d
ArbatosPakelis <tomasjuodelis123@gmail.com>
2023-04-17 16:32:37 +0300

    made all basic http requests work, added some error support

--- backend/index.js
+++ backend/index.js
@@ -83,18 +83,19 @@ app.get('/getPartSpec', (req, res) => {
 app.delete('/removePart/:id', (req, res) => {
 
     pool.getConnection((err, connection) => {
-        if(err) throw err
+        // if(err) throw err
         const sql = 'DELETE FROM detale WHERE id_Detale = ' + req.params.id;
         connection.query(sql, (err, rows) => {
             connection.release() // return the connection to pool
             if (!err) {
                 res.send(`part info with the record ID ${[req.params.id]} has been removed.`)
-            } else {
-                console.log(err)
+            } else if (err.errno == 1451) {
+                res.send('part is being used as a foreign key in other tables, delete those entries first')
             }
-            
-            console.log('The data from detale table is: \n', rows)
-            res.send(err)
+            else {
+                console.log(err)
+                res.send("error")
+            }
         })
     })
 });
@@ -104,7 +105,7 @@ app.delete('/removePart/:id', (req, res) => {
 app.delete('/removeSpecPart', (req, res) => {
 
     pool.getConnection((err, connection) => {
-        if(err) throw err
+        // if(err) throw err
         let tipas = req.query.tipas;
         let id = req.query.id;
         const sql = 'DELETE FROM ' + tipas.toLowerCase() +' WHERE id_' + tipas + ' = ' + id;
@@ -112,14 +113,195 @@ app.delete('/removeSpecPart', (req, res) => {
             connection.release() // return the connection to pool
             if (!err) {
                 res.send(`part specialized info with the record ID ${[id]} has been removed.`)
-            } else {
+            }
+            else if (err.errno == 1451) {
+                res.send('part is being used as a foreign key in other tables, delete those entries first')
+            }
+            else {
                 console.log(err)
             }
             
-            console.log('The data from detale table is: \n', rows)
-            res.send(err)
+            console.log(err)
+            res.send("error")
         })
     })
 });
+
+// insert general part info
+// pavyzdys url
+// addPart?gamintojas=kazka&pavadinimas=kazka&kaina=100&aprasymas=kazka&isleidimo_data=2023-04-02&kiekis=2&spalva=juoda&tipas=Procesorius&id_Detale=3
+app.post('/addPart', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        
+        const params = req.query // get all params
+        const keys = Object.keys(params) // get param names
+        let line1 = ""
+        for (a in keys){ // merge params names into a single string, but still has a extra comma at the end
+            line1 += keys[a] + ","
+        }
+        let line2 = ""
+        for (a in keys){ // merge data into a single string, but still has a extra comma at the end
+            var entry = params[keys[a]]
+            if (entry.match(/^\d+$/)){ // regex check if text can be a proper number
+                line2 +=  entry + ","
+            }
+            else{
+                line2 +=  '\''+ entry + "\',"
+            }
+        }
+        const sql = 'INSERT INTO detale(' + line1.substring(0, line1.length - 1) + ') VALUES (' + line2.substring(0, line2.length - 1) + ');'
+        console.log(sql)
+        connection.query(sql, (err, rows) => {
+        connection.release() // return the connection to pool
+        if (!err) {
+            res.send(`detale has been added.`)
+        }
+        else if (err.errno == 1062) {
+            res.send('douplicate entry, try to think of new id')
+        }
+        else {
+            console.log(err)
+            res.send(err)
+        }
+
+        })
+    })
+});
+
+// insert general part info
+app.post('/addSpecPart', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        
+        let params = req.query
+        const iranga = params["iranga"] // get the type of part we're making
+        let keys = Object.keys(params)
+        let line1 = ""
+        for (a in keys){ // besides the first value, merge params names into a single string, but still has a extra comma at the end
+            if(a != 0){
+                line1 += keys[a] + ","
+            }
+        }
+        let line2 = ""
+        for (a in keys){ // besides the first value, merge data into a single string, but still has a extra comma at the end
+            if(a != 0){
+                var entry =params[keys[a]]
+                if (entry.match(/^\d+$/)){ // regex check if text can be a proper number
+                    line2 +=  entry + ","
+                }
+                else{
+                    line2 +=  '\''+ entry + "\',"
+                }
+            }
+        }
+        const sql = 'INSERT INTO ' + iranga +'(' + line1.substring(0, line1.length - 1) + ') VALUES (' + line2.substring(0, line2.length - 1) + ');'
+        console.log(sql)
+        connection.query(sql, (err, rows) => {
+        connection.release() // return the connection to pool
+        if (!err) {
+            res.send(`detale has been added.`)
+        }
+        else if (err.errno == 1062) {
+            res.send('douplicate entry, try to think of new id')
+        }
+        else {
+            console.log(err)
+            res.send(err)
+        }
+
+        })
+    })
+});
+
+// Update a general parts info
+app.put('/setPart', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        
+        const params = req.query // get all params
+        const keys = Object.keys(params) // get param names
+        let line1 = ""
+        let name = ""
+        let id = -1
+        for (a in keys){ // merge data into a single string, but still has a extra comma at the end
+            if(!keys[a].startsWith("id")){
+                var entry = params[keys[a]]
+                if (entry.match(/^\d+$/)){ // regex check if text can be a proper number
+                    line1 +=  keys[a] + " = " + entry + ", "
+                }
+                else{
+                    line1 +=  keys[a] + " = " + '\''+ entry + "\', "
+                }
+            }
+            else{
+                name = keys[a]
+                id = params[keys[a]]
+            }
+        }
+        const sql = 'UPDATE detale SET ' + line1.substring(0, line1.length - 2) + ' WHERE ' + name + " = " + id + ';'
+        connection.query(sql , (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(`Part has been updated`)
+            } else {
+                console.log(err)
+                res.send(err)
+            }
+
+        })
+    })
+})
+
+
+// Update a general parts info
+app.put('/setSpecPart', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        
+        let params = req.query
+        const iranga = params["iranga"] // get the type of part we're making
+        let keys = Object.keys(params)
+        let line1 = ""
+        let name = ""
+        let id = -1
+        for (a in keys){ // merge data into a single string, but still has a extra comma at the end
+            if(a != 0){
+                if(!keys[a].startsWith("id")){
+                    var entry = params[keys[a]]
+                    if (entry.match(/^\d+$/)){ // regex check if text can be a proper number
+                        line1 +=  keys[a] + " = " + entry + ", "
+                    }
+                    else{
+                        line1 +=  keys[a] + " = " + '\''+ entry + "\', "
+                    }
+                }
+                else{
+                    name = keys[a]
+                    id = params[keys[a]]
+                }
+            }
+        }
+        const sql = 'UPDATE ' + iranga + ' SET ' + line1.substring(0, line1.length - 2) + ' WHERE ' + name + " = " + id + ';'
+        connection.query(sql , (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if(!err) {
+                res.send(`Part has been updated`)
+            } else {
+                console.log(err)
+                res.send(err)
+            }
+
+        })
+    })
+})
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
